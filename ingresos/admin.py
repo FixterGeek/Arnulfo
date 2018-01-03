43eec05679eb227e54154a0d9a23983b6a18217e
JoseLAmador <josel.amador.s@gmail.com>
2018-01-03 16:55:51 -0600

    :punch:

--- ingresos/admin.py
+++ ingresos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Sale, SaleItem, Client
 
+admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
+admin.site.register(Client)
 # Register your models here.
