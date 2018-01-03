8fad56aed564b79311cf510c6f6514abb8b9487a
JoseLAmador <josel.amador.s@gmail.com>
2018-01-03 15:10:40 -0600

    :trollface:

--- egresos/admin.py
+++ egresos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Purchase, Provider, Product
+from .models import Purchase, Provider, Product, PurchaseItem
 
 admin.site.register(Product)
 admin.site.register(Provider)
 admin.site.register(Purchase)
+admin.site.register(PurchaseItem)
 
--- egresos/models.py
+++ egresos/models.py
@@ -1,20 +1,28 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 
 class Provider(models.Model):
     provider = models.CharField(max_length=140)
     address = models.CharField(max_length=140)
-    email = models.CharField(max_length=140)
-    phone = models.IntegerField()
+    email = models.EmailField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,10}$',
+                                 message="El número de teléfono debe ingresarse en el formato: '7751234567'. Hasta 10 dígitos permitidos.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True)
 
     def __str__(self):
         return self.provider
 
 
 class Purchase (models.Model):
+    PAYMENTS = (('TC', 'Tarjeta Credito'),
+                ('Efectivo', 'Efectivo'),
+                ('TD', 'Tarjeta Debito'))
+    created = models.DateTimeField(auto_now_add=True)
     provider = models.ForeignKey(Provider, related_name="provid", on_delete=models.CASCADE)
-    units = models.IntegerField()
-    uprice = models.DecimalField(max_digits=10, decimal_places=2)
+    units = models.PositiveIntegerField(default=1)
+    kg_total = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False)
+    payment = models.CharField(max_length=100, choices=PAYMENTS, )
     total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
@@ -24,7 +32,23 @@ class Purchase (models.Model):
 class Product(models.Model):
     name = models.CharField(max_length= 140)
     category = models.CharField(max_length = 140)
-    units = models.IntegerField()
+    uprice = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+#Prueba
+
+class PurchaseItem(models.Model):
+    order = models.ForeignKey(Purchase, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    #price = models.DecimalField(max_digits=10, decimal_places=2)
+    weigth = models.DecimalField(max_digits=10, decimal_places=2)
+    animal_ref = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.product.uprice*self.weigth
+
