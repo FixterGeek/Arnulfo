be2de3a0beba164e839446a4fbf7314d61f357f5
Oswaldinho <oswalfut_96@hotmail.com>
2018-01-10 12:35:56 -0600

    ya

--- adminArnu/settings.py
+++ adminArnu/settings.py
@@ -164,8 +164,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)  
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
@@ -174,3 +172,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (  
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
