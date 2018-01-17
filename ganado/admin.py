5ee789e7a070646f7603395dce95379e279b295a
JoseLAmador <josel.amador.s@gmail.com>
2018-01-16 18:01:53 -0600

    Modelo Peso

--- ganado/admin.py
+++ ganado/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Animal, Lote, Corral, GastoAnimal
+from .models import Animal, Lote, Corral, GastoAnimal, Peso
 
 admin.site.register(Lote)
 admin.site.register(Animal)
 admin.site.register(Corral)
 admin.site.register(GastoAnimal)
+admin.site.register(Peso)
 # Register your models here.
 
