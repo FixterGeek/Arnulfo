9f3745a4eb4206e1bc507eed8a25ec7c34a47a3f
JoseLAmador <josel.amador.s@gmail.com>
2018-01-22 17:00:12 -0600

    Serializer

--- adminArnu/urls.py
+++ adminArnu/urls.py
@@ -22,12 +22,14 @@ from django.views.static import serve
 
 from ganado import urls as ganadoUrls
 from accounts import urls as authUrls
+from egresos import urls as egresosUrls
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('api/ganado/', include(ganadoUrls, namespace='ganado-urls')),
     path('api/auth/', include(authUrls, namespace='auth-urls')),
+    path('api/egresos/', include(egresosUrls, namespace='egresos-urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(
         regex=r'^media/(?P<path>.*)$',
