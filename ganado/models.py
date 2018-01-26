816fae57a965f588482eacd297d88a7346c75262
Oswaldinho <oswalfut_96@hotmail.com>
2018-01-25 21:10:31 -0600

    search and filter animal

--- ganado/models.py
+++ ganado/models.py
@@ -31,7 +31,7 @@ class Corral(models.Model):
 class Lote(models.Model):
     name = models.CharField(max_length=130)
     status = models.BooleanField(default=True)
-    corral = models.ForeignKey(Corral, related_name='lotes', on_delete=models.CASCADE, blank=True, null=True)
+    corral = models.OneToOneField(Corral, related_name='lotes', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -68,13 +68,13 @@ class Animal(models.Model):
     color = models.CharField(max_length=150)
     comentarios = models.TextField()
     owner = models.CharField(max_length=150)
-    lote = models.ForeignKey(Lote, related_name='animals', on_delete=models.CASCADE, blank=True, null=True)
+    lote = models.ForeignKey(Lote, related_name='animals', on_delete=models.SET_NULL, blank=True, null=True)
     tipo_animal = models.CharField(max_length=100, choices=TIPOA, blank=True, null=True)
     
     status = models.BooleanField(default=True)
     costo_inicial = models.DecimalField(max_digits=10, decimal_places=2)
-    fierro_original = models.ImageField(upload_to='fierrosO/')
-    fierro_nuevo = models.ImageField(upload_to='fierrosN/')
+    fierro_original = models.ImageField(upload_to='fierrosO/', blank=True, null=True)
+    fierro_nuevo = models.ImageField(upload_to='fierrosN/', blank=True, null=True)
     ref_factura_original = models.CharField(max_length=100)
 
     # numero_semana = models.PositiveIntegerField(default=0, blank=True, null=True)
@@ -91,7 +91,7 @@ class GastoAnimal(models.Model):
     ('Alimento','Alimento'),
     ('Vacuna', 'Vacuna'))
 
-    animal = models.ForeignKey(Animal, related_name='aliments', on_delete=models.CASCADE, null=True, blank=True)
+    animal = models.ForeignKey(Animal, related_name='aliments', on_delete=models.PROTECT, null=True, blank=True)
     costo = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.PositiveIntegerField(default=0)
     tipo = models.CharField(max_length=100, choices=TIPO, blank=True, null=True)
@@ -102,7 +102,7 @@ class GastoAnimal(models.Model):
         return "Gasto no. {}, {} ".format(self.id, self.tipo)
 
 class Peso(models.Model):
-  created = models.DateTimeField(auto_now_add=True)
+  created = models.DateField(auto_now_add=True)
   peso = models.DecimalField(max_digits=10, decimal_places=2)
   animal = models.ForeignKey(Animal, related_name='pesadas', on_delete=models.PROTECT)
 
--- ganado/views.py
+++ ganado/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import AnimalSerializer, CorralSerializer, LoteSerializer, AlimentoSerializer, BasicAnimalSerializer, BasicLoteSerializer
-from .models import Animal, Lote, GastoAnimal, Corral
+from .serializers import AnimalSerializer, CorralSerializer, LoteSerializer, AlimentoSerializer, BasicAnimalSerializer, BasicLoteSerializer, PesoSerializer, BasicPesoSerializer
+from .models import Animal, Lote, GastoAnimal, Corral, Peso
 from .pagination import AnimalPagination
+from django.db.models import Q
 
 
 #VIews for the API
@@ -19,9 +20,24 @@ class AnimalViewSet(viewsets.ModelViewSet):
 			return AnimalSerializer
 		return BasicAnimalSerializer 
 
+	def get_queryset(self, *args, **kwargs):
+		query = self.request.GET.get("q")
+		lote_query = self.request.GET.get("lote")
+		queryset_list = super(AnimalViewSet,self).get_queryset()
+		if query:
+			queryset_list = queryset_list.filter(
+				Q(arete_rancho__icontains=query)|
+				Q(arete_siniga__icontains=query)
+				).distinct()
+		if lote_query:
+			queryset_list = queryset_list.filter(lote=lote_query)
+		return queryset_list
+
+
+
 class LoteViewSet(viewsets.ModelViewSet):
 	queryset = Lote.objects.all()
-	serializer_class = LoteSerializer
+	#serializer_class = LoteSerializer
 
 	def get_serializer_class(self):
 		if self.action == 'list':
@@ -37,3 +53,14 @@ class CorralViewSet(viewsets.ModelViewSet):
 class AlimentoViewSet(viewsets.ModelViewSet):
 	queryset = GastoAnimal.objects.all()
 	serializer_class = AlimentoSerializer
+
+class PesoViewSet(viewsets.ModelViewSet):
+	queryset = Peso.objects.all()
+	#serializer_class = PesoSerializer
+
+	def get_serializer_class(self):
+		if self.action == 'list':
+			return PesoSerializer
+		if self.action == 'retrieve':
+			return PesoSerializer
+		return BasicPesoSerializer 
