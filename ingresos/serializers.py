5ca5cc1482043e547f409151e5f90b7cc566a4a1
JoseLAmador <josel.amador.s@gmail.com>
2018-01-29 18:26:22 -0600

    ingresos

--- ingresos/serializers.py
+++ ingresos/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Client
+from .models import Client, Sale
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
+        fields = '__all__'
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sale
         fields = '__all__'
 
--- ingresos/views.py
+++ ingresos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import ClientSerializer
-from .models import Client
+from .serializers import ClientSerializer, SaleSerializer
+from .models import Client, Sale
 
 #VIews for the API
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
