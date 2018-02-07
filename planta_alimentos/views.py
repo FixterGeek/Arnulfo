b540acbf535045fb33dc6af30550f0d8a4ab4d60
Miguel Gonzalez <miglez90290995@gmail.com>
2018-02-07 12:32:21 -0600

    Changing the way to retrieve and post serializers (Planta Alimentos)

--- planta_alimentos/views.py
+++ planta_alimentos/views.py
@@ -17,6 +17,26 @@ from django.db.models import Q
 class InsumoViewSet(viewsets.ModelViewSet):
     queryset = Insumo.objects.all()
     serializer_class = InsumoSerializer
+    pagination_class = PlantaAlimentosPagination
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return InsumoSerializer
+        if self.action == 'retrieve':
+            return InsumoSerializer
+        return BasicInsumoSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        query = self.request.GET.get("q")
+        provider_query = self.request.GET.get("provider")
+        queryset_list = super(InsumoViewSet, self).get_queryset()
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(name__icontains=query)
+            ).distinct()
+        if provider_query:
+            queryset_list = queryset_list.filter(provider=provider_query)
+        return queryset_list
 
 
 class ItemViewSet(viewsets.ModelViewSet):
