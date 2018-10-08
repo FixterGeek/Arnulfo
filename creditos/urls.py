c1572de8f2c2d1dda3478cfc71447296f4eb0a91
Oswaldinho24k <oswalfut_96@hotmail.com>
2018-10-07 22:49:23 -0500

    lel

--- creditos/urls.py
+++ creditos/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.conf.urls import url
 from django.urls import path, include
-from .views import AcreedorViewSet, DisposicionViewSet
+from .views import AcreedorViewSet, DisposicionViewSet, ReciboViewSet
 
 router = routers.DefaultRouter()
 router.register('acreedores', AcreedorViewSet)
 router.register('disposiciones', DisposicionViewSet)
+router.register('recibos', ReciboViewSet)
 
 
 
@@ -13,6 +14,5 @@ app_name='creditos'
 
 urlpatterns=[
 	url('^', include(router.urls)),
-	
-	# url(r'^animalApi/$',AnimalAPI.as_view() )
+		
 ]
