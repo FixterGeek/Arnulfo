b67f48ff241e5f54926ea6971924b58a2d44ed1f
Oswaldinho24k <oswalfut_96@hotmail.com>
2018-10-22 14:06:29 -0500

    solved animal relation at GastoAnimal Serializer

--- creditos/models.py
+++ creditos/models.py
@@ -42,7 +42,7 @@ class Recibo(models.Model):
 	capital = models.DecimalField(max_digits=10, decimal_places=2)
 	intereses = models.DecimalField(max_digits=10, decimal_places=2)
 	saldo = models.DecimalField(max_digits=10, decimal_places=2)
-	fecha = models.DateTimeField(auto_now_add=False, db_index=True)
+	fecha = models.DateField(auto_now_add=False, db_index=True)
 
 	def __unicode__(self):
 		return self.id
